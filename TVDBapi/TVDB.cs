676e4806ad57b09279aa0e2edc4cdf7a382b2498
jrow <jeremiah.towe@cchsfs.com>
2016-08-03 13:08:52 -0400

    begin using flurl

--- TVDBapi/TVDB.cs
+++ TVDBapi/TVDB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Flurl.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,16 @@ namespace TVDBapi
     public class TVDB
     {
         public string test;
+
+        public async Task<string> getStuff()
+        {
+            var responseString = await "https://api.thetvdb.com/login"
+                .WithHeader("Accept", "application/json")
+                .PostJsonAsync(new { apikey = "26EF26F60843B42C"})
+                .ReceiveString();
+            return responseString;
+        }
+
+
     }
 }
